2a6671389f3f07c93c916d3b6823b02cb7f6c4b1
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-03-25 08:43:32 -0400

    Backup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Chat from './components/Chat.js';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Container>
+          <Header />
+            <Switch>
+              <Route path="/room">
+                <Chat />
+              </Route>
+              <Route>
+                <Route path="/">
+                  <Login />
+                </Route>
+              </Route>
+            </Switch>
+          </Container>
+        </Router>
       </header>
     </div>
   );
